9c115101ec1d1d63766db174881c053631ee34a9
samersawiers <141464182+samersawiers@users.noreply.github.com>
2024-11-17 20:11:25 +0200

    Add files via upload

---
+++ Task 4.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Question
+{
+    public string QuestionBody { get; set; }
+    public int Marks { get; set; }
+
+    public Question(string body, int marks)
+    {
+        QuestionBody = body;
+        Marks = marks;
+    }
+
+    public override string ToString()
+    {
+        return "Question: " + QuestionBody + " (Marks: " + Marks + ")";
+    }
+}
+
+class Answer
+{
+    public int Num { get; set; }
+    public string ChooseText { get; set; }
+
+    public Answer(int num, string text)
+    {
+        Num = num;
+        ChooseText = text;
+    }
+
+    public override string ToString()
+    {
+        return "Answer " + Num + ": " + ChooseText;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<Question> questions = new List<Question>
+        {
+            new Question("What is OOP?", 5),
+            new Question("Explain polymorphism.", 10)
+        };
+
+        Console.WriteLine("Questions List:");
+        foreach (var question in questions)
+        {
+            Console.WriteLine(question.ToString());
+        }
+
+        Dictionary<Question, List<Answer>> exam = new Dictionary<Question, List<Answer>>();
+
+        List<Answer> answers1 = new List<Answer>
+        {
+            new Answer(1, "Object-Oriented Programming"),
+            new Answer(2, "Overloading and Inheritance")
+        };
+        exam[questions[0]] = answers1;
+
+        List<Answer> answers2 = new List<Answer>
+        {
+            new Answer(1, "Allows objects to share functionality."),
+            new Answer(2, "Enhances code reusability.")
+        };
+        exam[questions[1]] = answers2;
+
+        Console.WriteLine("\nExam Dictionary:");
+        foreach (var entry in exam)
+        {
+            Console.WriteLine(entry.Key.ToString());
+            foreach (var answer in entry.Value)
+            {
+                Console.WriteLine("  " + answer.ToString());
+            }
+        }
+    }
+}
---
+++ Task 5.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public Employee(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
+    public override string ToString()
+    {
+        return "Employee ID: " + Id + ", Name: " + Name;
+    }
+}
+
+public class Club
+{
+    private List<Employee> clubMembers = new List<Employee>();
+
+    public void AddInClub(Employee emp)
+    {
+        clubMembers.Add(emp);
+        Console.WriteLine(emp.Name + " has been added to the club.");
+    }
+}
+
+public class SocialInsurance
+{
+    private List<Employee> insuredEmployees = new List<Employee>();
+
+    public void BeginSocialInsurance(Employee emp)
+    {
+        insuredEmployees.Add(emp);
+        Console.WriteLine(emp.Name + " has been enrolled in social insurance.");
+    }
+}
+
+public class Company
+{
+    private List<Employee> employees = new List<Employee>();
+    private Club c = new Club();
+    private SocialInsurance socialIns = new SocialInsurance();
+
+    public void Add(Employee emp)
+    {
+        employees.Add(emp);
+        Console.WriteLine(emp.Name + " has been added to the company.");
+
+        c.AddInClub(emp);
+        socialIns.BeginSocialInsurance(emp);
+    }
+
+    public void DisplayEmployees()
+    {
+        Console.WriteLine("Employees in the company:");
+        foreach (var emp in employees)
+        {
+            Console.WriteLine(emp);
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Company company = new Company();
+
+        Employee emp1 = new Employee(1, "samer");
+        Employee emp2 = new Employee(2, "sawiers");
+
+        company.Add(emp1);
+        company.Add(emp2);
+
+        Console.WriteLine();
+        company.DisplayEmployees();
+    }
+}
